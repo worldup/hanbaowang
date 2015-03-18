04654e64e6049c950495c9821d594405211c4523
lili <worldup@163.com>
2015-03-18 22:37:12 +0800

    提交模板

--- source/burgerKing/src/main/java/com/upbest/JxlsTest.java
+++ source/burgerKing/src/main/java/com/upbest/JxlsTest.java
@@ -1,9 +1,13 @@
 package com.upbest;
 
 import net.sf.jxls.transformer.XLSTransformer;
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,10 +16,24 @@ import java.util.Map;
  */
 public class JxlsTest {
     public static void main(String[] args) throws IOException, InvalidFormatException {
+        ClassPathResource classPathResource=new ClassPathResource("template\\REV.xlsx");
+        String path= classPathResource.getFile().getAbsolutePath();
+
         Map beans = new HashMap();
-        beans.put("name", "aaaaa");
-        beans.put("bb", "1111");
+        Map title=new HashMap();
+        title.put("date",new Date().toString());
+        title.put("manager","lili");
+        title.put("ev","lili");
+        title.put("restNo","1234");
+        Map body=new HashMap();
+        Map b01=new HashMap();
+        b01.put("score","22");
+        b01.put("Y","11");
+        body.put("b01",b01);
+        beans.put("title",title);
+        beans.put("body",body);
         XLSTransformer transformer = new XLSTransformer();
-        transformer.transformXLS("d:\\ccc.xlsx", beans, "d:\\ccc1.xlsx");
+
+        transformer.transformXLS(path, beans, "c:\\develop\\ccc1.xlsx");
     }
 }
