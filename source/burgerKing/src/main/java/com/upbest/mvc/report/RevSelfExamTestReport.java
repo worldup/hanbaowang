22fc74bbf581e08344273119947f7c9c4354a21a
李力 <li_li@chexiang.com>
2015-03-22 22:59:05 +0800

    修改REV提交

--- source/burgerKing/src/main/java/com/upbest/mvc/report/RevSelfExamTestReport.java
+++ source/burgerKing/src/main/java/com/upbest/mvc/report/RevSelfExamTestReport.java
@@ -1,11 +1,17 @@
 package com.upbest.mvc.report;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.upbest.mvc.vo.ExamDetailInfoVO;
+import net.sf.jett.transform.ExcelTransformer;
+import org.apache.commons.lang.StringUtils;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.CollectionUtils;
 
 import com.upbest.mvc.service.IExamService;
@@ -148,5 +154,149 @@ public class RevSelfExamTestReport extends ExamTestReport {
 		
 		return cellValue;
 	}
+    private Map<String,String> moduleMaps(){
+        Map<String,String> moduleMap=new HashMap();
+        moduleMap.put("友好","youhao");
+        moduleMap.put("sos","sos");
+        moduleMap.put("清洁","qingjie");
+        moduleMap.put("品质","pinzhi");
+        moduleMap.put("盈利值班管理","yingli");
+        moduleMap.put("训练","xunlian");
+        moduleMap.put("维护保养","weihu");
+        moduleMap.put("食品安全","shipin");
+        moduleMap.put("评估总结","pingguzongjie");
+        return moduleMap;
+    }
+    private Map transJXlsMap(String fullServerPath,ExamDetailInfoVO vo){
+        Map jxlsMap=new HashMap();
+        Map heads=new HashMap();
+        Map modules=new HashMap();
+        jxlsMap.put("heads",heads);
+        jxlsMap.put("modules",modules);
+        List<Field> headList=vo.getHeads();
+        //转换标头
+        if(org.apache.commons.collections.CollectionUtils.isNotEmpty(headList)){
+            for(Field item:headList){
+                if("日期".equals(item.getFieldName())){
+                    heads.put("date", item.getFieldValue()) ;
+                }
+                else if("值班经理".equals(item.getFieldName())){
+                    heads.put("manager", item.getFieldValue()) ;
+                }
+                else if("评估人".equals(item.getFieldName())){
+                    heads.put("feedback", item.getFieldValue()) ;
+                }
+                else if("餐厅编号".equals(item.getFieldName())){
+                    heads.put("restName", item.getFieldValue()) ;
+                }
+            }
+        }
+        List<ExamDetailInfoVO.Module> moduleList= vo.getModules();
+        if(org.apache.commons.collections.CollectionUtils.isNotEmpty(moduleList)){
+            Map<String,String> moduleMaps=moduleMaps();
+            for(ExamDetailInfoVO.Module module:moduleList){
+                // module.getFields();
+                Map qingjiewaibu=new HashMap();
+                modules.put(moduleMaps.get(module.getName()),qingjiewaibu);
+                Map qingjiewaibuquestions=new HashMap();
+                qingjiewaibu.put("questions",qingjiewaibuquestions);
+                List<ExamDetailInfoVO.Question> questions=  module.getQuestions();
+                if(org.apache.commons.collections.CollectionUtils.isNotEmpty(questions)){
+                    for(ExamDetailInfoVO.Question question:questions){
+                        Map C01=new HashMap();
+                        if("评估总结".equals(module.getName())){
+                            if("我最欣赏这家餐厅".equals(question.getQuesDesc())){
+                                qingjiewaibuquestions.put("pg_xinshang",C01);
+                            }
+                            else if("最需关注的问题点".equals(question.getQuesDesc())){
+                                qingjiewaibuquestions.put("pg_guanzhu",C01);
+                            }
 
+                        }else{
+                            qingjiewaibuquestions.put(question.getSerialNumber(),C01);
+                        }
+                        C01.put("score",question.getScore());
+                        ExamDetailInfoVO.QuestionTestDetailInfo questionTestDetailInfo= question.getTestDetailInfo();
+                        Map testDetailInfoMap=new HashMap();
+                        C01.put("testDetailInfo",testDetailInfoMap);
+                        testDetailInfoMap.put("score",questionTestDetailInfo.getScore());
+                        List<Map<String,String>> evidenceList=new ArrayList();
+                        String evidenceStr=questionTestDetailInfo.getEvidence();
+                        testDetailInfoMap.put("evidence", evidenceList);
+                        if(StringUtils.isNotEmpty(evidenceStr)){
+                            String[] evidenceStrArr=     StringUtils.split(evidenceStr,",");
+                            int tempIdx=0;
+                            for(String tempEv:evidenceStrArr){
+                                Map<String,String> temMap=new HashMap();
+                                temMap.put("address", fullServerPath + tempEv);
+                                temMap.put("value","图片"+(++tempIdx));
+                                evidenceList.add(temMap);
+                            }
+                        }
+
+                        Map fieldInfosMap=new HashMap();
+                        testDetailInfoMap.put("fieldInfos",fieldInfosMap);
+                        List<Field> fieldList= questionTestDetailInfo.getFieldInfos();
+                        if(org.apache.commons.collections.CollectionUtils.isNotEmpty(fieldList)){
+                            for(Field field:fieldList){
+                                if("Y".equals(field.getFieldName())){
+                                    if("1".equals(field.getFieldValue())){
+                                        fieldInfosMap.put("Y","√");
+                                    }
+                                    else{
+                                        fieldInfosMap.put("Y","");
+                                    }
+
+                                }else if("N".equals(field.getFieldName())){
+                                    if("1".equals(field.getFieldValue())){
+                                        fieldInfosMap.put("N","√");
+                                    }
+                                    else{
+                                        fieldInfosMap.put("N","");
+                                    }
+
+                                }else if("不适用".equals(field.getFieldName())){
+                                    if("1".equals(field.getFieldValue())){
+                                        fieldInfosMap.put("NA","√");
+                                    }
+                                    else{
+                                        fieldInfosMap.put("NA","");
+                                    }
+                                }else if("描述问题".equals(field.getFieldName())){
+                                    fieldInfosMap.put("DESC",field.getFieldValue());
+                                }
+                                else if("分值".equals(field.getFieldName())){
+                                    boolean isNa=false;
+                                    for(Field temp:fieldList){
+                                        if("不适用".equals(temp.getFieldName()) && "1".equals(temp.getFieldValue())){
+                                            isNa=true;
+                                        }
+                                    }
+                                    fieldInfosMap.put("SCORE",isNa?"N/A":field.getFieldValue());
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+        return jxlsMap;
+
+    }
+    public byte[]   generateExcel(String fullServerPath,ExamDetailInfoVO examDetailInfo){
+        ClassPathResource classPathResource=new ClassPathResource("template\\REV.xlsx");
+
+        Map beans=transJXlsMap(fullServerPath,examDetailInfo);
+        ExcelTransformer transformer = new ExcelTransformer();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try{
+            Workbook workbook= transformer.transform(classPathResource.getInputStream(), beans);
+            workbook.write(outputStream);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return outputStream.toByteArray();
+    }
 }
