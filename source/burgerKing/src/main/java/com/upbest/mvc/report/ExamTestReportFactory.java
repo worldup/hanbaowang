904c3f5c157163d6493f696745cf31dc24e22b08
李力 <li_li@chexiang.com>
2015-03-30 17:21:38 +0800

    提交通用表单邮件发送功能

--- source/burgerKing/src/main/java/com/upbest/mvc/report/ExamTestReportFactory.java
+++ source/burgerKing/src/main/java/com/upbest/mvc/report/ExamTestReportFactory.java
@@ -19,6 +19,8 @@ public class ExamTestReportFactory {
 		}else if(type.contains("打烊")){
 			report = new DayangExamTestReport(service);
 		}
+		else
+		report=new DayangExamTestReport(service);
 		
 		return report;
 	}
